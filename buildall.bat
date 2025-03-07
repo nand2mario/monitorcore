db4f96e504be9283cf39d668370d2c25302f33a2
nand2mario <nand2mario@outlook.com>
2025-03-07 21:56:46 +0800

    fix build files

--- buildall.bat
+++ buildall.bat
@@ -1,51 +1,43 @@
 
+@echo off
 
-set GWSH=..\..\Gowin_V1.9.10.03_x64\IDE\bin\gw_sh
 
-echo
-echo "============ Building console60k with snes controller ==============="
-echo
-%GWSH% build.tcl console60k snes
+set GWSH=\Gowin\Gowin_V1.9.10.03_x64\IDE\bin\gw_sh
+@REM set GWSH_NEW=\Gowin\Gowin_V1.9.10.03_x64\IDE\bin\gw_sh
 
-echo
-echo "============ Building console60k with ds2 controller ==============="
-echo
-%GWSH% build.tcl console60k ds2
+@REM echo. 
+@REM echo ============ Building nano20k ===============
+@REM echo.
+@REM %GWSH% build.tcl nano20k
 
-echo
-echo "============ Building mega60k with snes controller ==============="
-echo
-%GWSH% build.tcl mega60k snes
+@REM echo.
+@REM echo ============ Building primer25k with snes/nes controller ===============
+@REM echo.
+@REM %GWSH% build.tcl primer25k snes
 
-echo
-echo "============ Building mega60k with ds2 controller ==============="
-echo
-%GWSH% build.tcl mega60k ds2
-
-echo
-echo "============ Building nano20k ==============="
-echo
-%GWSH% build.tcl nano20k
-
-echo
-echo "============ Building primer25k with snes controller ==============="
-echo
-%GWSH% build.tcl primer25k snes
-
-echo
-echo "============ Building primer25k with ds2 controller ==============="
-echo
+echo.
+echo ============ Building primer25k with ds2 controller ===============
+echo.
 %GWSH% build.tcl primer25k ds2
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo
-echo "============ Building mega138k pro with snes controller ==============="
-echo
-%GWSH% build.tcl mega138k snes
+echo.
+echo ============ Building mega60k with ds2 controller ===============
+echo.
+%GWSH% build.tcl mega60k ds2
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo
-echo "============ Building mega138k pro with ds2 controller ==============="
-echo
+echo.
+echo ============ Building mega138k with ds2 controller ===============
+echo.
 %GWSH% build.tcl mega138k ds2
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo.
+echo ============ Building console60k with ds2 controller ===============
+echo.
+%GWSH% build.tcl console60k ds2
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 dir impl\pnr\*.fs
 
